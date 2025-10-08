61135cc04a7e8f629ec118cbda6d0c0947609a54
jalmontegit <jalmonte721@gmail.com>
2025-10-07 20:50:28 -0400

    lab started

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,20 @@
+import { use, useState } from 'react';
+
+const [books, setBooks] = useState([
+    { title: 'Book 1', author: 'Author 1' },
+    { title: 'Book 2', author: 'Author 2' },
+    { title: 'Book 3', author: 'Author 3' },
+]);
+
+const [newBook, setNewBook] = useState({ title: '', author: '' });
+
+
+<div className="bookshelfDiv">
+  <div className="formDiv">
+    <h3>Add a Book</h3>
+    {/* Form will go here */}
+  </div>
+  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+</div>
+
+export default App;
